93671918d5b9cd459156c0fea9e62166d7c70899
tonsmoothguru <amanigos@tonsmoothguru.com>
2025-08-16 14:11:11 +0800

    Client application updates

--- dating-app-client/src/app/app.ts
+++ dating-app-client/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,20 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('dating-app-client');
-  protected name = 'Tester';
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected readonly title = signal('Dating Client');
+  protected members = signal<any>([]);
+
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    }catch(error) {
+      throw error;
+    }
+  }
 }

2cfd290bb9456568b0a9ed7ac1cc13aa3a1c434a
tonsmoothguru <amanigos@tonsmoothguru.com>
2025-08-16 12:03:31 +0800

    Initial commit for client code

---
+++ dating-app-client/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected readonly title = signal('dating-app-client');
+  protected name = 'Tester';
+}

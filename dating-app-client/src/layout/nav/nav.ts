90e144ea3c1da8cff5cecde0c24214d71cc20cc7
tonsmoothguru <amanigos@tonsmoothguru.com>
2025-08-20 22:53:18 +0800

    Added angular routing

--- dating-app-client/src/layout/nav/nav.ts
+++ dating-app-client/src/layout/nav/nav.ts
@@ -1,29 +1,38 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { LoginCreds } from '../../types/user';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService = inject(AccountService);
+  private router = inject(Router);
+  private toast = inject(ToastService);
   protected creds = {} as LoginCreds;
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: (result) => {
-        console.log(result);
+      next: () => {
+        this.router.navigateByUrl('/members');
+        this.toast.success('Logged in successfully');
         this.creds = { email: '', password: '' };
       },
-      error: (error) => alert(error.message),
+      error: (error) => {
+        this.toast.error(error.error);
+      },
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
+    this.toast.info('User logged out');
   }
 }
